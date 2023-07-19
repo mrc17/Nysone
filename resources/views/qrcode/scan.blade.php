859fd5bf259d9294cc2003aa29881adb1d845cc7
Axel <axeltoure1eme@gmail.com>
2023-07-19 07:28:10 +0000

    Added QR Code Generation to HTML Page

---
+++ resources/views/qrcode/scan.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>QR Code</title>
+</head>
+
+<body>
+    <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                    alt="logo">
+                NysOne
+            </a>
+            <div
+                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="visible-print text-center">
+    {!! QrCode::size(100)->generate(Request::url()); !!}
+    <p>Scan me to return to the original page.</p>
+</div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
