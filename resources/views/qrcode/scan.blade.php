6a58715d20e00d9426441abb6cda7a5a10a6743b
Axel <axeltoure1eme@gmail.com>
2023-07-19 11:03:47 +0000

    Affichage du Qr code

--- resources/views/qrcode/scan.blade.php
+++ resources/views/qrcode/scan.blade.php
@@ -12,19 +12,18 @@
     <section class="bg-gray-50 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-                    alt="logo">
+                <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
                 NysOne
             </a>
-            <div
-                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div class="visible-print text-center">
-    {!! QrCode::size(100)->generate(Request::url()); !!}
-    <p>Scan me to return to the original page.</p>
-</div>
+            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 space-y-4 w-full flex justify-center align-content-center md:space-y-6 sm:p-8">
+                        {!! QrCode::size(400)->generate(session('qr_code_data')); !!}
+                    </div>
+                <p class="text-center pb-5 text-white font-weight-bolder">Scannez-moi pour contunier à la page .</p>
             </div>
         </div>
     </section>
 </body>
 
 </html>
+
