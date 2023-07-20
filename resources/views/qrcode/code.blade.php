7c24d2c0fd18f4dbd26a9ba2490495ac33f80cbf
Axel <axeltoure1eme@gmail.com>
2023-07-20 08:56:42 +0000

    Ajout pour vu accueil

--- resources/views/qrcode/code.blade.php
+++ resources/views/qrcode/code.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Connexion</title>
+    <title>verification | Code</title>
 </head>
 <body>
     <section class="bg-gray-50 dark:bg-gray-900">
@@ -24,7 +24,19 @@
                             <label for="Code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Code de Vérication</label>
                             <input type="number" name="Code" id="Code" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="045312" required="">
                         </div>
-                        <button type="submit" class="w-full text-white bg-green-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Contuiner</button>
+                        @if (isset($error))
+                        <div class="mb-4 rounded-lg bg-danger-100 text-base text-white text-center" role="alert">
+                            {{ $error }}
+                        </div>
+                        @endif
+
+                        @error('code')
+                        <div class="mb-4 rounded-lg bg-danger-100 text-base text-white text-center" role="alert">
+                            {{ $message }}
+                        </div>
+                        @enderror
+
+                        <button type="submit" class="w-full text-white bg-green-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Continuer</button>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">Vous n'avez pas de compte
                             <a href="{{route('inscription')}}" class="font-medium text-green-600 hover:underline dark:text-primary-500">Cliquez ici</a>
                         </p>
